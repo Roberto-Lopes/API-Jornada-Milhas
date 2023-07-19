b276cfd94d5da09cd47f699b3e3488c75415cfed
Roberto-Lopes <robertolopes6492@gmail.com>
2023-07-19 15:27:26 -0300

    criando classe Startup

---
+++ Startup.cs
@@ -0,0 +1,38 @@
+﻿using api_alura_challenge.Data;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql;
+
+namespace api_alura_challenge;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        var connectionString = Configuration.GetConnectionString("DepoimentoConnection");
+
+        services.AddDbContext<DepoimentoContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+        services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAnyOrigins",
+                builder =>
+                {
+                    builder.AllowAnyOrigin();
+                    builder.AllowAnyHeader();
+                    builder.AllowAnyMethod();
+                });
+        });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UseCors("AllowAnyOrigins");
+    }
+}
